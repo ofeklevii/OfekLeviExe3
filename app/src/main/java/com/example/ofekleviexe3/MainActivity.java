b3b604f785979727ee5db4fdec4c8bce93173fda
ofeklevii <ofeklevi@sisma.org.il>
2024-01-08 21:50:58 +0200

    finish

--- app/src/main/java/com/example/ofekleviexe3/MainActivity.java
+++ app/src/main/java/com/example/ofekleviexe3/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.ofekleviexe3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn;
+    int sum = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+    }
+
+    public void exe3(View view) {
+        btn.setText("This is a click number:" + sum);
+        sum++;
+        if (sum==6){
+            btn.setText("Enough to click. Go to new start!");
+            sum = 0;
     }
 }
